5ff29d2a726f891857affe67a08755d6891754dd
Gustavo Bona <91689075+GustavoBonaS@users.noreply.github.com>
2021-10-21 13:29:18 -0300

    att

--- src/Cone.java
+++ src/Cone.java
@@ -2,65 +2,101 @@ public class Cone {
 
 	private float raio;
 	private float altura;
-	private float tipo;
-	private double teorema;
-	
-	public Cone (float raio, float altura) {
+	private int tipo;
+	private int areatotal;
+	private int arealat;
+	private int areacir;
+
+
+	public Cone(float raio, float altura, int tipo) {
 		setRaio(raio);
 		setAltura(altura);
-	}
-	
-	public Cone() {
-		setAltura(altura);
+		setTipo(tipo);
 	}
 
-	private void setTipo(int tipo) {
-					
+	public Cone() {
+	}
+
+	public float getAltura() {
+		return altura;
 	}
 
 	public void setAltura(float altura) {
 		if (altura > 0)
 			this.altura = altura;
 	}
-	
-	public float getAltura() {
-		return altura;
-	}
 
 	public float getRaio() {
 		return raio;
 	}
+
 	public void setRaio(float raio) {
 		if (raio > 0)
 			this.raio = raio;
 	}
+
+	public float teorema() {
+		return ((raio * raio) + (altura * altura));
+	}
+
+	public float geratriz() {
+		return (float) Math.sqrt(teorema());
+	}
+
+	public float areatotal() {
+		return (3.14f * raio * (raio + geratriz()));
+	}
+
+	public float areacir() {
+		return 3.14f * raio * raio;
+	}
+
+	public float arealat() {
+		return 3.14f * raio * geratriz();
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
 	
-	public double teorema() {
-		return Math.sqrt((altura * altura) + (raio * raio));
+	public float litros() {
+			return (areacir + arealat + areatotal) * 18;
 	}
-	public double areatotal () {
-		return (3.14 * raio * (raio + teorema));		
-	}
-	public double areacir() {
-		return 3.14 * raio * raio;	
-	}
-	public double arealat() {
-		return 3.14 * raio * teorema;
-	}
-	
+
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("-->Cone");
-		builder.append("\nRaio: "+raio );
-		builder.append("\nAltura: "+altura);
-		builder.append("\nTipo: "+tipo);
+		builder.append("Cone [raio=");
+		builder.append(raio);
+		builder.append(", altura=");
+		builder.append(altura);
+		builder.append(", tipo=");
+		builder.append(tipo);
+		builder.append(", areatotal=");
+		builder.append(areatotal);
+		builder.append(", arealat=");
+		builder.append(arealat);
+		builder.append(", areacir=");
+		builder.append(areacir);
+		builder.append(", teorema()=");
+		builder.append(teorema());
+		builder.append(", geratriz()=");
+		builder.append(geratriz());
+		builder.append(", areatotal()=");
+		builder.append(areatotal());
+		builder.append(", areacir()=");
+		builder.append(areacir());
+		builder.append(", arealat()=");
+		builder.append(arealat());
+		builder.append(", litros()=");
+		builder.append(litros());
+		builder.append("]");
 		return builder.toString();
-		
 	}
-	
-	
-	
-	
-	
+
+
 }
--- src/ConeMain.java
+++ src/ConeMain.java
@@ -1,23 +1,12 @@
-import java.util.Scanner;
 
 public class ConeMain {
 
 	public static void main(String[] args) {
-		Scanner ent = new Scanner(System.in);
 		
-		Cone cone = new Cone();
-		System.out.println("Insira o raio: ");
-		cone.setRaio(ent.nextFloat());
-		System.out.println(cone);
+		Cone c = new Cone(6 , 8 , 1);
+		System.out.println(c);
 		
-		Cone cone2 = new Cone();
-		System.out.println("Insira  a altura: ");
-		cone2.setRaio(ent.nextFloat());
-		
-		System.out.println("Geratriz: "+cone.teorema());
-		System.out.println("Area total: "+cone.areatotal());
 
-		
 	}
 
 }

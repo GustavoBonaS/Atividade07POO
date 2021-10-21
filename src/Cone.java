379e1a54966ffc52a9811bbea718227c0ca7d255
Gustavo Bona <gustavobosiqueira@gmail.com>
2021-10-21 16:24:39 -0300

    att f

--- src/Cone.java
+++ src/Cone.java
@@ -3,9 +3,6 @@ public class Cone {
 	private float raio;
 	private float altura;
 	private int tipo;
-	private int areatotal;
-	private int arealat;
-	private int areacir;
 
 
 	public Cone(float raio, float altura, int tipo) {
@@ -62,40 +59,38 @@ public class Cone {
 	public void setTipo(int tipo) {
 		this.tipo = tipo;
 	}
+	public float i() {
+		return (float) (1/3.45);
+	}
 	
 	public float litros() {
-			return (areacir + arealat + areatotal) * 18;
+			return (areatotal() / i());
 	}
+	
+	public int latas() {
+		return (int) Math.ceil((litros()) / 18);
+	}
+	public double preco() {
+		if (tipo == 1) {
+			return (238.90);
+		}else if (tipo == 2) {
+			return (467.98);
+		}else {
+			return (758.34);
+		}
+			
+	}
+	
+	public double precototal() {
+		return (double) (latas() * preco());
+	}
+	
 
 	@Override
 	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("Cone [raio=");
-		builder.append(raio);
-		builder.append(", altura=");
-		builder.append(altura);
-		builder.append(", tipo=");
-		builder.append(tipo);
-		builder.append(", areatotal=");
-		builder.append(areatotal);
-		builder.append(", arealat=");
-		builder.append(arealat);
-		builder.append(", areacir=");
-		builder.append(areacir);
-		builder.append(", teorema()=");
-		builder.append(teorema());
-		builder.append(", geratriz()=");
-		builder.append(geratriz());
-		builder.append(", areatotal()=");
-		builder.append(areatotal());
-		builder.append(", areacir()=");
-		builder.append(areacir());
-		builder.append(", arealat()=");
-		builder.append(arealat());
-		builder.append(", litros()=");
-		builder.append(litros());
-		builder.append("]");
-		return builder.toString();
+		return "Cone \nraio= " + raio + "\naltura= " + altura + "\ntipo= " + tipo + "\ngeratriz()= " + geratriz()
+				+ "\narea total= " + areatotal() + "\narea do fundo= " + areacir() + "\narea lateral= " + arealat()
+				+ "\nlitros= " + litros() + "\nlatas()=" + latas() + "\npreco total= " + precototal();
 	}
 
 
